a93722118bdee60fb062c0662293f488cd82ebde
Adnan0061 <adnan.ahmed0061@gmail.com>
2021-10-05 23:28:00 +0600

    final

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,20 @@
-import { faFacebookSquare, faInstagramSquare, faYoutubeSquare } from '@fortawesome/free-brands-svg-icons';
+import { faAccusoft, faFacebookSquare, faInstagramSquare, faYoutubeSquare } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Navbar } from 'react-bootstrap';
 import { Heart } from 'react-bootstrap-icons';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
         <div style={{backgroundColor: '#38A9C5'}}>
-            <Container  className='d-flex justify-content-between py-3'>
+            <Container>
+                <div className='py-3'>
+                <Navbar.Brand  as={Link} to={"/home"} style={{color:'white', fontSize: '36px',}}><FontAwesomeIcon icon={faAccusoft} size='lg' className='me-2' color='white'></FontAwesomeIcon> Future Leaders</Navbar.Brand>
+                <h6 style={{color:'white'}}>Your trusted online learning platform</h6>
+                </div>
+            </Container>
+            <Container  className='d-flex justify-content-between py-3 border-top border-light'>
                 <div>
                     <p className='my-0 pt-1 text-light'>Copyright ©2021 All rights reserved | This is made with <Heart></Heart> by Adnan Ahmed</p>
                 </div>
